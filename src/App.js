77e01081a25447611fb54f8873087bd0450de692
Nick-2003 <55626561+Nick-2003@users.noreply.github.com>
2022-06-13 13:49:39 +0800

    q

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ import TextField from '@material-ui/core/TextField';
 import { makeStyles, ThemeProvider, createTheme } from '@material-ui/core/styles';
 
 import "fontsource-roboto";
-import { Typography } from '@material-ui/core';
-import { Container } from '@material-ui/core';
+import { Typography, Container, Paper, Grid } from '@material-ui/core';
+
 
 import { borderRadius } from '@mui/system';
 import { withThemeCreator } from '@material-ui/styles';
@@ -78,71 +78,84 @@ function ChEx() {
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
-        <header className="App-header">
-          <Typography variant="h3">Whalecome</Typography>
-          <Typography variant="body2" component="div">to Using MUI</Typography> {/* div with h2 styling applied */}
-          <Typography variant="subtitle1">Here are some examples</Typography>
-          <ButtonStyled />
-          <ButtonGroup 
-            variant="contained">
-            <Button 
-              OnClick={()=>alert("DLROW OLLEH")}
-              href="H3110_W0R1D" 
-              startIcon={<SaveIcon />}
-              color="primary">
-              HELLO WORLD
-            </Button>
-            <Button 
-              OnClick={()=>alert("ECAPS EYBDOOG")}
-              href="600D8Y3_5P4C3" 
-              endIcon={<DeleteIcon />}
-              color="secondary">
-              GOODBYE SPACE
-            </Button>
-          </ButtonGroup>
-          <TextField 
-            variant="filled"
-            type="date"
-            color="secondary"
-          />
-          <TextField 
-            variant="outlined"
-            type="text"
-            color="primary"
-            label="Label"
-            placeholder="Text"
-          />
-          <TextField 
-            variant="outlined"
-            type="color"
-            color="secondary"
-          />
-          {/* </Checkbox> */}
-          {/* <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a> */}
-        </header>
-        <body>
-          <div>
-            <FormControlLabel
-            control={
-              <ChEx />
-            }
-            label="You'll pay for your insulin"
-            /> 
-          </div>
-          <img src={logo} className="App-logo" alt="logo" />
-        </body>
-      </div>
+      <Container maxWidth="md">
+        <div className="App">
+          <header className="App-header">
+            <Typography variant="h3">Whalecome</Typography>
+            <Typography variant="body2" component="div">to Using MUI</Typography> {/* div with h2 styling applied */}
+            <Typography variant="subtitle1">Here are some examples</Typography>
+            <ButtonStyled />
+            <ButtonGroup 
+              variant="contained">
+              <Button 
+                OnClick={()=>alert("DLROW OLLEH")}
+                href="H3110_W0R1D" 
+                startIcon={<SaveIcon />}
+                color="secondary">
+                HELLO WORLD
+              </Button>
+              <Button 
+                OnClick={()=>alert("ECAPS EYBDOOG")}
+                href="600D8Y3_5P4C3" 
+                endIcon={<DeleteIcon />}
+                color="primary">
+                GOODBYE SPACE
+              </Button>
+            </ButtonGroup>
+            <Grid container spacing ={1} justify="center">
+              <Grid item>
+                <Paper style={{ height: 100, width: 100, marginBottom: 20, marginTop: 20}}/>
+              </Grid>
+              <Grid item>
+                <Paper style={{ height: 100, width: 100, marginBottom: 20, marginTop: 20}}/>
+              </Grid>
+              <Grid item>
+                <Paper style={{ height: 100, width: 100, marginBottom: 20, marginTop: 20}}/>
+              </Grid>
+            </Grid>
+            <TextField 
+              variant="filled"
+              type="date"
+              color="secondary"
+            />
+            <TextField 
+              variant="outlined"
+              type="text"
+              color="primary"
+              label="Label"
+              placeholder="Text"
+            />
+            <TextField 
+              variant="outlined"
+              type="color"
+              color="secondary"
+            />
+            {/* </Checkbox> */}
+            {/* <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a> */}
+          </header>
+          <body>
+            <div>
+              <FormControlLabel
+              control={
+                <ChEx />
+              }
+              label="You'll pay for your insulin"
+              /> 
+            </div>
+            <img src={logo} className="App-logo" alt="logo" />
+          </body>
+        </div>
+      </Container>
     </ThemeProvider>
   );
 }
